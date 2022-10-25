c68dd137320ee5299f97e79181ea1db85a3784c0
Paul Hultgren <pjhultgren5@gmail.com>
2022-10-24 17:46:49 -0700

    fix: dont republish same version

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,9 +1,21 @@
 import fs from 'fs'
+import fetch from 'node-fetch'
 import { execSync } from 'child_process'
 
 const packages = fs.readdirSync('./dist', { withFileTypes: true }).filter((dirent) => dirent.isDirectory())
 
-for (let { name } of packages) {
+for await (let { name } of packages) {
+    const pkg = JSON.parse(fs.readFileSync(`./dist/${name}/package.json`).toString('utf-8'))
+
+    const res = await fetch('https://registry.npmjs.org/' + pkg.name + '/latest')
+
+    const { version } = await res.json()
+
+    if (version === pkg.version) {
+        console.log(`Package ${pkg.name} is already published with version ${version}, skipping...`)
+        continue
+    }
+
     try {
         execSync('npm publish --access public', { cwd: `./dist/${name}` })
     } catch (error) {
